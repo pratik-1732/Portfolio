b0151ce4549fc6df03a70a4a40a11411cdac482f
Pratik <patilpratikp035@gmail.com>
2025-07-13 17:03:50 +0530

    added padding to card

--- Personal Site/components/SkillCard.jsx
+++ Personal Site/components/SkillCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SkillCard = ({ title, image }) => {
   return (
-    <div className="flex flex-col items-center px-5 py-7 bg-gray-800 rounded-lg shadow-lg border border-yellow-300 hover:cursor-pointer hover:border-blue-700 hover:shadow-sky-700 hover:scale-110 duration-300 transition-transform">
+    <div className="flex flex-col items-center p-12 bg-gray-800 rounded-lg shadow-lg border border-yellow-300 hover:cursor-pointer hover:border-blue-700 hover:shadow-sky-700 hover:scale-110 duration-300 transition-transform">
       <img src={image} className="h-20 w-20" />
       <h3 className="text-xl text-white font-semibold tracking-wide mt-5">
         {title}
